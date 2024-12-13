8ca859f9321442bed8a8512a32a45536a7114089
dry914 <dradryg@gmail.com>
2024-12-13 15:27:33 +0300

    chore: add simple contract

--- test/helpers/SimpleContract.sol
+++ test/helpers/SimpleContract.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.23;
 
 contract SimpleContract {
     mapping(address account => bool isCaller) private s_magicMapping;
+    mapping(address account => uint256 balance) public s_balances;
 
     function call() external {
         s_magicMapping[msg.sender] = true;
@@ -11,4 +12,10 @@ contract SimpleContract {
     function getMagicValue(address acc) external view returns (bool isCaller) {
         return s_magicMapping[acc];
     }
+
+    function deposit() external payable {
+        s_balances[msg.sender] += msg.value;
+    }
+
+    receive() external payable virtual {}
 }
